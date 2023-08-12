26c5c82d65f5bdfc36a78319f34abfb2698e6270
yurihondo <yurihondo.dev@gmail.com>
2023-08-12 06:20:17 +0000

    Deploying to github-pages from  @ c724d3b601c709a111d3f05cd393d73a258aab66 🚀

--- settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-rootProject.name = "page-simplestreaming"
-
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
-}
